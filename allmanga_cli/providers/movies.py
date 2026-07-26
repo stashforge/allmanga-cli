93ffdee828a7d410eafdc3660f6b4c870d2a759c
Stashforge <grabswalker@gmail.com>
2026-07-26 08:29:16 +0530

    chore: reorder primary sources to prioritize Hollymoviehd at Rank 0 and move VidSrc to the end of the primary tier

--- allmanga_cli/providers/movies.py
+++ allmanga_cli/providers/movies.py
@@ -216,9 +216,9 @@ class MoviesProvider(MovieProvider):
 
         # 3 highly reliable Primary sources
         primary_pool = [
-            lambda: self._fetch_vidsrc(media_type, tmdb_id, s, e),
+            lambda: self._fetch_vidnest_endpoint("hollymoviehd", media_type, tmdb_id, s, e),
             lambda: self._fetch_vidnest_endpoint("moviebox", media_type, tmdb_id, s, e),
-            lambda: self._fetch_vidnest_endpoint("hollymoviehd", media_type, tmdb_id, s, e)
+            lambda: self._fetch_vidsrc(media_type, tmdb_id, s, e)
         ]
         
         # 12 experimental Backup sources
